232b86e2724995b286a9321d0f9d47959e6ecc60
charan-crafts <mangalacharancharan@gmail.com>
2025-09-19 11:14:01 +0530

    added the peak of mountain

---
+++ DSA/BinarySearch/PeekOfMountain.java
@@ -0,0 +1,27 @@
+
+public class PeekOfMountain {
+
+    public static int peakIndex(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int mid = (start+end)/2;
+
+            if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        
+        int arr[]={0,2,3,4,5,2,1,0};
+
+        System.out.println(peakIndex(arr));
+        
+    }
+}
