5043a31973bb9b4bb2a9f072afd5c8513572580c
charan-crafts <mangalacharancharan@gmail.com>
2025-09-23 15:09:12 +0530

    count zeros

---
+++ DSA/Recursion/CountZeros.java
@@ -0,0 +1,20 @@
+
+public class CountZeros {
+    
+    
+
+    public static int countNumberOfZeros(int number,int count){
+
+        int remainder = number %10;
+        int quotient = number / 10;
+
+        if(quotient==0){
+            return count;
+        }
+        
+        return countNumberOfZeros(quotient,remainder==0?++count:count);
+    }
+    public static void main(String[] args) {
+        System.out.println(countNumberOfZeros(102020202,0));
+    }
+}
