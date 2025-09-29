9a78eb8850bd8cb9863912bd8a5d772a90f1e983
charan-crafts <mangalacharancharan@gmail.com>
2025-09-29 22:42:38 +0530

    Insertion at end and start

--- kunal/src/main/java/com/charan/LinkedList/LinkedList.java
+++ kunal/src/main/java/com/charan/LinkedList/LinkedList.java
@@ -8,27 +8,82 @@ public class LinkedList {
 
     private Node tail;
 
-    public LinkedList(){
+    public LinkedList() {
 
         this.size = 0;
     }
 
     // Insert the Node at beginning
 
-    public void insertAtStarting(int data){
+    public void insertAtStarting(int data) {
 
         // Create the Node
+
+        Node newNode = new Node(data);
+
+        // Check if head is null than it is first Node
+
+        if (head == null) {
+
+            head = newNode;
+
+            tail = head;
+            return;
+        }
+
+        newNode.next = head;
+
+        head = newNode;
+
+        size++;
+
+    }
+
+    public void display() {
+
+        Node temp = head;
+
+        while (temp != null) {
+
+            System.out.print(temp.data + "-->");
+
+            temp = temp.next;
+        }
+        System.out.println("End");
+    }
+
+    public void insertAtEnd(int data) {
+
+        Node newNode = new Node(data);
+
+        if(tail == null ){
+
+            head = newNode;
+            tail= head;
+        }
+
+        tail.next = newNode;
+
+        tail=newNode;
+
+        size++;
+
 
 
     }
 
-    class Node{
+    public void size(){
+        System.out.println(size);
+    }
+
+    public void insertAt
+    class Node {
 
         private int data;
 
         private Node next;
 
-        public Node(int data){
+        public Node(int data) {
 
             this.data = data;
         }
---
+++ kunal/src/main/java/com/charan/LinkedList/Main.java
@@ -0,0 +1,20 @@
+package com.charan.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.insertAtStarting(20);
+
+        linkedList.insertAtStarting(10);
+
+        linkedList.insertAtEnd(100);
+
+        linkedList.insertAtEnd(120);
+
+        linkedList.display();
+
+    }
+}
